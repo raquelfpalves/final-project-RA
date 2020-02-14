5ee00c6ee2e02e1872695d10e18ebaef6e5c83fc
raquelfpalves <raquelfpalves@gmail.com>
2020-02-14 14:22:16 +0000

    search form JS

--- index.js
+++ index.js
@@ -43,7 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "75fb66e318950ecccacc7fe99c8be68b";
-let city = "Lisbon";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "75fb66e318950ecccacc7fe99c8be68b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+document.addEventListener("submit", handleSubmit);
